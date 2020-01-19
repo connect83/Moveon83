9a3d4f6f78e0ac3ef2154fcad0163f3f9cf71a76
little <littletoes83@gmail.com>
2020-01-18 19:18:12 -0600

    this is my first commit

---
+++ src/test/java/Qaclickacademy/appiumTest.java
@@ -0,0 +1,16 @@
+package Qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class appiumTest {
+	@Test
+	public void mobileapp()
+	{
+		System.out.println("mobileapp");
+	}
+	@Test
+	public void iosapp()
+	{
+		System.out.println("iosapp");
+	}
+}
---
+++ src/test/java/Qaclickacademy/seleniumTest.java
@@ -0,0 +1,17 @@
+package Qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class seleniumTest {
+@Test
+public void Browserautomation()
+{
+	System.out.println("browserautomation");
+}
+@Test
+public void elementsui()
+{
+	System.out.println("elementsui");
+}
+
+}
