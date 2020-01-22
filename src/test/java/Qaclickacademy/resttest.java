1945c004e28bcff03e36211365c311d2797ce56e
little <littletoes83@gmail.com>
2020-01-22 10:04:55 -0600

    new restapi added

---
+++ src/test/java/Qaclickacademy/resttest.java
@@ -0,0 +1,24 @@
+package Qaclickacademy;
+import org.testng.annotations.Test;
+public class resttest {
+
+	@Test
+	public void rest1()
+	{
+		System.out.println("restapp");
+		System.out.println("restapp1");
+		System.out.println("restapp2");
+	}
+	public void rest2()
+	{
+		System.out.println("1restapp");
+		System.out.println("1restapp1");
+		System.out.println("1restapp2");
+	}
+	public void rest3()
+	{
+		System.out.println("2restapp");
+		System.out.println("2restapp1");
+		System.out.println("2restapp2");
+	}
+}
